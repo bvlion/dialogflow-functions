a4f430144bc2fb8cf2c1b7e021ba963272f743cc
Satoshi Iwai <24517539+bvlion@users.noreply.github.com>
2025-06-24 21:28:58 +0900

    ファンを夏仕様に変更

--- functions/intents/infrared.js
+++ functions/intents/infrared.js
@@ -24,9 +24,9 @@ async function livingOff(admin, execSend) {
 }
 
 async function livingSet(admin, execSend) {
-  const fan = 'fan_1'
-  // const urlNames = ['living_light', fan, 'fan_reverse', 'fan_off', fan, 'fan_reverse'] // 夏場
-  const urlNames = ['living_light', fan] // 冬場
+  const fan = 'fan_2'
+  const urlNames = ['living_light', fan, 'fan_reverse', 'fan_off', fan, 'fan_reverse'] // 夏場
+  // const urlNames = ['living_light', fan] // 冬場
 
   const results = await remo(admin, urlNames)
 
